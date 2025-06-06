d575d92d428e58c98f37f1442184567e4716e93d
MichaelObvious <michele.palese42@outlook.it>
2025-06-06 22:00:22 +0200

    prayers

---
+++ data.js
@@ -0,0 +1,335 @@
+let items = [
+    { title: 'Dalla liturgia bizantina', text: 'Insieme a Maria e a tutti i santi, mettiamo noi stessi, i nostri amici e tutte le nostre giornate nelle mani di Gesù. Amen.' },
+    { title: 'Salmo 4', text: `Quando ti chiamo, Dio, rispondimi, perché so che sei buono e giusto.
+Quando ero triste, tu mi hai aiutato.
+Tu mi ascolti e hai cura di me.
+
+Perché mi offendete?
+Perché amate cose senza senso?
+Perché vi piace dire bugie?
+  
+Il Signore fa cose meravigliose per chi si fida di lui.
+Il Signore mi ascolta quando lo chiamo.  
+  
+Fermatevi e non fate più il male:
+quando siete a letto, ascoltate cosa succede nel vostro cuore.
+  
+Amate come vi ama Dio,
+e abbiate fiducia in lui.
+  
+Molti dicono: "Chi ci farà vedere il bene,  
+se tu, Signore, sei lontano da noi?".  
+  
+Tu, Dio, hai messo più gioia nel mio cuore  
+di quanta ne diano montagne di grano e barili di vino.  
+  
+In pace vado a letto e subito mi addormento,  
+perché tu solo, Signore, mi fai sentire al sicuro e tranquillo.
+
+Tua è la gloria, Padre, Figlio e Spirito Santo, ora e sempre e nei secoli dei secoli. Amen.` },
+    { title: 'Salmo 30 [29]', text: `Grazie Signore, perché ti curi di me come una mamma.
+Ho avuto paura, mi hai fatto coraggio.
+Ero in pericolo, mi hai difeso.
+Stavo male, mi hai curato.
+Mi hai salvato quando ero giù,
+non mi hai lasciato cadere in un luogo buio.
+
+Cantate, voi amici di Dio,
+raccontate quanto è buono il Signore!
+Quando ti arrabbi, è per pochi minuti.
+Quando mi vuoi bene, mi vuoi bene per sempre.
+Se di sera mi addormento triste,
+al mattino mi fai svegliare con il cuore contento.
+  
+Ero felice, pensavo di esserlo per sempre.
+Ma tu hai girato la faccia da un'altra parte,
+e la felicità non so più dov'è.
+Adesso sono triste. Lo sei anche tu.
+  
+Ma non smettere di guardarmi
+e nulla di male mi accadrà.
+Tienimi nei tuoi occhi,
+tu proteggi la mia vita meglio di me.
+Tienimi nei tuoi occhi,
+la mia tristezza imparerà a fare salti di gioia.
+E canterò, canterò
+per dirti grazie.
+
+A te la gloria, Padre, Figlio e Spirito santo, ora e sempre e nei secoli dei secoli. Amen.` },
+    { title: 'Salmo 91 [90] (da spezzare)', text: `Chi abita al riparo di Dio dorme tranquillo,
+il Signore veglia su di lui nella notte.
+
+Io dico: «Sei il mio rifugio,
+tu mi proteggi, mio Dio,
+io mi fido di te.»
+
+Tu mi rispondi:
+«Non ti lascerò prendere nella rete come un uccello.
+Non ti lascerò cadere nelle mani di chi ti vuol male.
+
+Ti terrò al riparo sotto le mie ali.
+Al caldo tra le mie piume.
+Ti starò vicino e ti difenderò.
+
+Non permetterò che tu abbia paura né di giorno, né di notte.
+Vedrai mille che cadranno, ma nulla colpirà te.
+Vedrai il mostro portarne via ancora di più, ma tu non sarai neppure sfiorato.
+I cattivi li vedrai precipitare.
+
+Tu hai detto. "Sì, mio rifugio sei tu, o Signore!", ti sei affidato a me.
+Il mio nome ti sta attorno come una tenda.
+Nulla di male colpirà te, quelli che ami eChi abita al riparo di Dio dorme tranquillo,
+il Signore veglia su di lui nella notte.
+
+Io dico: «Sei il mio rifugio,
+tu mi proteggi, mio Dio,
+io mi fido di te.»
+
+Tu mi rispondi:
+«Non ti lascerò prendere nella rete come un uccello.
+Non ti lascerò cadere nelle mani di chi ti vuol male.
+
+Ti terrò al riparo sotto le mie ali.
+Al caldo tra le mie piume.
+Ti starò vicino e ti difenderò.
+
+Non permetterò che tu abbia paura né di giorno, né di notte.
+Vedrai mille che cadranno, ma nulla colpirà te.
+Vedrai il mostro portarne via ancora di più, ma tu non sarai neppure sfiorato.
+I cattivi li vedrai precipitare.
+
+Tu hai detto. "Sì, mio rifugio sei tu, o Signore!", ti sei affidato a me.
+Il mio nome ti sta attorno come una tenda.
+Nulla di male colpirà te, quelli che ami e la tua casa.
+
+I miei angeli cammineranno con te.
+Ti alzeranno in volo, dove troppe sono le pietre ed è facile inciampare.
+Senza paura passerai tra vipere e leoni.
+
+I leoni e i draghi li cavalcherai. La tua fiducia in me ti rende invincibile.
+
+Chiamami e ti ascolterò.
+Di' il mio nome, ti risponderò.
+
+Se nella rete cadrai come un uccello, io sarò con te nella rete.
+Se chi ti vuol male allungherà le mani su te, prenderà anche me.
+Se avrai paura, sarò con te ad avere paura.
+Se piangerai, con te piangerò.
+Se le lacrime non smetteranno, a una a una le asciugherò.
+Se il mostro ti prenderà, starò con te nelle grinfie del mostro.
+
+Ti voglio libero e salvo.
+Ti voglio nei giorni felice.
+Dentro la vita ti voglio.»
+
+Tua è la gloria, Padre, Figlio e Spirito Santo, ora e sempre e nei secoli dei secoli. Amen. la tua casa.
+
+Ti voglio libero e salvo.
+Ti voglio nei giorni felice.
+Dentro la vita ti voglio.»
+
+Tua è la gloria, Padre, Figlio e Spirito Santo, ora e sempre e nei secoli dei secoli. Amen.` },
+    { title: 'Salmo 104 [103] (estratto)', text: `Grande, tu sei grande  
+Della luce hai fatto il tuo mantello  
+La Terra hai messo al suo posto  
+E l’hai avvolta nelle acque come in un manto 
+
+Hai aperto le sorgenti, scavato le valli  
+Dai monti fai scorrere i fiumi  
+dove gli animali lasciano la loro sete  
+Agli uccelli che volano hai dato il cielo  
+e i rami degli alberi, dove cantano
+
+Con le acque dei monti bagni i campi  
+Con le acque dei cieli bagni i monti  
+Disseti la Terra dove ha sete  
+La sfami dove ha fame  
+Ti prendi cura degli animali  
+Ti prendi cura degli uomini
+
+È bello, è molto bello  
+E intorno, quante creature  
+Ne è colma la terra  
+E colmo il mare senza confini
+Ogni cosa vive del tuo respiro  
+E splendente è la faccia della Terra
+Grande, tu sei grande, o Dio
+
+Benedetto sei Tu, Padre, Figlio e Spirito Santo, da sempre e per sempre. Amen, amen.` },
+    { title: 'Preghiera quotidiana di Antonio di Surož', text: `O Dio, ti ringrazio per questo nuovo giorno.
+Anche se non non dovessi sentirmi contento, io ti ringrazio.
+Tu hai fatto questo nuovo giorno e me l'hai donato.
+Io sono tuo e questo giorno è tuo.
+Tu l'hai creato totalmente nuovo, ancora intatto, tutto puro e l'hai messo davanti a me.
+Ti prego, Dio, benedici questo giorno.
+Benedici ogni cosa, benedici ogni persona che incontrerò, benedici ogni situazione in cui mi troverò.
+Non farmi dimenticare che tu sei sempre con me. Insieme a te sono pronto a affrontare ogni cosa.
+Amen.` },
+    { title: 'Sotto la tua protezione', text: `Sotto la tua protezione cerchiamo rifugio, santa Madre di Dio. Non disprezzare le suppliche di noi che siamo nella prova, e liberaci da ogni pericolo, o Vergine gloriosa, e benedetta.` },
+    { title: 'Preghiera degli startzi di Optino', text: `Signore, donami un cuore sereno per accogliere tutto ciò che mi porterà questo giorno.
+Aiutami a fidarmi completamente di Te.
+In ogni momento stammi vicino, guidami e aiutami in tutto.
+Insegnami ad ascoltare con calma ogni notizia che mi arriva, avendo fiducia in Te.
+Guida i miei pensieri e i miei sentimenti quando parlo e in tutto quello che faccio.
+Quando succede qualcosa di inaspettato, ricordami che Tu sei sempre con me.
+Insegnami a comportarmi in modo giusto e saggio con la mia famiglia, senza far arrabbiare o addolorare nessuno.
+Signore, dammi la forza per affrontare le fatiche di oggi e ogni cosa che mi aspetta.
+Guida la mia volontà e insegnami a pregare, a credere, a sperare, a sopportare, a perdonare e ad amare.
+Amen.` },
+    { title: 'Benedizione dei Kohanim (Nm 6)', text: `Ci benedica il Signore  
+e ci custodisca.  
+Il Signore faccia risplendere per noi il suo volto  
+e ci faccia grazia.  
+Il Signore rivolga a noi il suo volto  
+e ci conceda pace.` },
+    { title: 'Chiediamo la carità (dalla liturgia siriaca)', text: `Dio e Signore dell'universo,
+Regna anche nei noChiediamo la carità (dalla liturgia siriaca)stri cuori.
+Quando litighiamo, insegnaci a costruire la pace.
+Quando diciamo bugie, insegnaci a volerci bene.
+Quando ci facciamo del male, insegnaci a perdonarci.
+Insegnaci a stare insieme, a stare in pace, a volerci bene.
+Rendici uniti, rendici forti.
+Insegnaci ad ascoltarti, quando parli e quando taci.
+La tua parola è il nostro tesoro,
+che tu ci parli è la nostra dolcezza,
+più dolce del miele è la tua parola.
+Sta' in mezzo a noi, Signore Gesù. Amen.` },
+    { title: 'Luce gioiosa', text: `Gesù Cristo, luce gioiosa della gloria santa del Padre. Giunti al tramonto del sole, lodiamo il Padre, il Figlio e lo Spirito Santo. Tu, Figlio di Dio che doni la vita, sei degno in ogni momento di essere lodato. Tutto l'universo ti dà gloria. Amen.` },
+    { title: 'Preghiera di Sant\'Efrem il siro', text: `Dio mio e Signore della mia vita,
+liberami dallo spirito della pigrizia,
+dello scoraggiamento
+dal desiderio di potere
+e dalle parole vane.
+
+Ma donami lo spirito di purezza
+di umiltà
+di pazienza
+e di amore.
+
+O mio Dio e mio Re,
+fa' che io veda i miei peccati
+e che non giudichi mio fratello
+perché Tu sei benedetto
+nei secoli dei secoli. Amen!` },
+    { title: 'Preghiera di San Nicolao', text: `Mio Signore e mio Dio, togli da me quanto mi allontana da te.
+Mio Signore e mio Dio, togli da me quanto mi allontana da te.
+
+Mio Signore e mio Dio, dammi tutto ciò che mi conduce a te.
+
+Mio Signore e mio Dio, toglimi a me, e dammi tutto a te.
+Mio Signore e mio Dio, dammi tutto ciò che mi conduce a te.
+
+Mio Signore e mio Dio, toglimi a me, e dammi tutto a te.` },
+    { title: 'Preghiera semplice di San Francesco', text: `Signore, fa di me uno strumento della tua pace:
+
+dove è odio, fa ch'io porti amore,
+dove è offesa, ch'io porti il perdono,
+dov'è discordia ch'io porti l'unione,
+
+dov'è dubbio fa' ch'io porti la fede,
+dove è l'errore, ch'io porti la verità,
+dove è la disperazione, ch'io porti la speranza.
+
+Dove è tristezza, ch'io porti la gioia,
+dove sono le tenebre, ch'io porti la luce.
+
+
+Maestro, fa che io non cerchi tanto
+di essere compreso, quanto di comprendere,
+di essere amato, quanto di amare.
+
+Poiché è dando, che si riceve,
+perdonando che si è perdonati,
+morendo che si risuscita a vita eterna.
+
+Amen.` },
+    { title: 'Preghiera quotidiana di Filarete di Mosca', text: `Signore, non so che cosa chiederti. Tu solo sai di cosa ho bisogno. Tu mi ami più di quanto io sia capace di amare me stesso.
+Il mio cuore è aperto a te: tu vedi i miei bisogni che io stesso non conosco. Vedi e fa' secondo la tua misericordia.
+Mi offro in completamente a te. Non ho altro desiderio che fare la tua volontà. Insegnami a pregare, prega tu in me.
+Amen.` },
+    { title: 'Salmo 138 [137]', text: `Ti ringrazio, Signore, con tutto il cuore:  
+hai ascoltato le parole della mia bocca.
+Non agli dèi, ma a te voglio cantare,  
+mi inchino davanti al tuo tempio santo.  
+
+Ti ringrazio perché mi ami, perché sei fedele:
+Tu ti ricordi quello che prometti.
+  
+Quanto ti ho chiamato, mi hai risposto,  
+mi hai dato forza. 
+Tutti i grandi della terra ti ringrazieranno, Signore,  
+quando sentiranno le parole della tua bocca.
+Capiranno quanto sei meraviglioso.
+  
+Canteranno le vie del Signore:  
+grande è il Signore!  
+Perché altissimo è il Signore, ma non si dimentica del piccolo e dell'umile;  
+il prepotente invece lo riconosce da lontano.  
+  
+Se cammino in mezzo al pericolo,  
+tu mi ridoni di vita;  
+contro la rabbia di chi mi vuole male stendi la tua mano
+e la tua mano mi rialza.  
+  
+Il Signore si prende cura di me.  
+Signore, il tuo amore è per sempre:  
+non abbandonare tutto ciò che hai fatto.
+
+Tua è la gloria, Padre, Figlio e Spirito Santo, ora e sempre e nei secoli dei secoli. Amen.` },
+    { title: 'Cantico di Simeone (Lc 2)', text: `Ora puoi lasciare, o Signore, che il tuo servo  
+vada in pace, secondo la tua parola,  
+perché i miei occhi hanno visto la tua salvezza,  
+preparata da te davanti a tutti i popoli:  
+luce per rivelarti alle genti  
+e gloria del tuo popolo.
+
+Gloria al Padre e al Figlio,
+e allo Spirito Santo,
+come era nel principio e ora e sempre,
+nei secoli dei secoli. Amen.` },
+    { title: 'Salmo 23 [22]', text: `Il Signore è il mio pastore:
+non manco di nulla.
+
+Su pascoli erbosi mi fa riposare,
+ad acque tranquille mi conduce.
+
+Rinfranca l'anima mia,
+mi guida per il giusto cammino
+a motivo del suo nome.
+
+Anche se vado per una valle oscura,
+non temo alcun male, perché tu sei con me.
+Il tuo bastone e il tuo vincastro
+mi danno sicurezza.
+
+Davanti a me tu prepari una mensa
+sotto gli occhi dei miei nemici.
+Ungi di olio il mio capo;
+il mio calice trabocca.
+
+Sì, bontà e fedeltà mi saranno compagne
+tutti i giorni della mia vita,
+abiterò ancora nella casa del Signore
+per lunghi giorni.` },
+    { title: 'Salmo 121 [120]', text: `Alzo gli occhi verso i monti:
+da dove mi verrà l'aiuto?
+Il mio aiuto viene dal Signore:
+egli ha fatto cielo e terra.
+
+Non lascerà vacillare il tuo piede,
+non si addormenterà il tuo custode.
+Non si addormenterà, non prenderà sonno
+il custode d'Israele.
+
+Il Signore è il tuo custode,
+il Signore è la tua ombra
+e sta alla tua destra.
+
+Di giorno non ti colpirà il sole,
+né la luna di notte.
+Il Signore ti custodirà da ogni male:
+egli custodirà la tua vita.
+
+Il Signore ti custodirà quando esci e quando entri,
+da ora e per sempre.` },
+];
